e87d2c4c48fd85e5278a57be7dc288e27696978d
Safira Raissa Rahmi <126964183+safiraissa231@users.noreply.github.com>
2024-02-18 01:45:08 +0700

    Update assessment_1_prompt_engineering.py

--- assessment_1_prompt_engineering.py
+++ assessment_1_prompt_engineering.py
@@ -27,7 +27,7 @@ prompts = {
 
               [INST]I have a discussion text in Bahasa Indonesia:""",
       "Llama-2-7B-32K-Instruct": """[INST]Write a concise summary of the discussion text, return your responses with 5 lines of indirect sentences that cover the key points of the discussion text in Bahasa Indonesia.[\INST]""",
-      "Qwen1.5-1.8B-Chat": """Write a concise summary of a discussion given by user"""
+      "Qwen1.5-1.8B-Chat": """Write a concise summary of the discussion text, return your responses with 5 lines of indirect sentences that cover the key points of the discussion text in Bahasa Indonesia."""
 }
 
 # Model identifiers mapping.
